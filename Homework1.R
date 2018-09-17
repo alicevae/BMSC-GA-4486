c45dc65b7a66b048406a2a2d340693054d4e942d
Alice <alicevae@users.noreply.github.com>
2018-09-17 16:36:00 -0400

    from slides

---
+++ Homework1.R
@@ -0,0 +1,17 @@
+#Assign the string hello world to object h and print the result.
+h <- "hello world"
+h
+
+objects()
+date()
+#Objects lists the names of variables, whereas Date lists a string showing the date
+
+#seq: lets you list numbers in a sequence from x to y. 
+#You can also pick what units you want to go by, or how many numbers you want to list total
+#Examples:
+simple <- seq(1,10)
+by_0.5 <- seq(1,10,by = 0.5)
+fifteen_total <- seq(1,length.out = 15)
+simple
+by_0.5
+fifteen_total
